bb723fcdf50ee7ddaef269ae5c50a44104385dd1
Etienne Cointet <etienne@cointet.com>
2021-06-23 17:29:43 +0200

    Jason feature

--- nginx/src/auth-module.php
+++ nginx/src/auth-module.php
@@ -1,7 +1,7 @@
 <?php
 //Working on different version
-$auth_version=1; // app Version (1 or 2)
-$auth_icon="";
-$auth_message=""; // Landing page
+$auth_version=2; // app Version (1 or 2)
+$auth_icon="https://pbs.twimg.com/profile_images/491236932572020738/ukRoi6nT_400x400.jpeg";
+$auth_message="Jason's WebSite!"; // Landing page
 $my_link=""; // your link
 ?>
