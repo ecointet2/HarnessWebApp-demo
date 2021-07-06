b15d8076d5189aa24bbcb4f7cdeb600dfc8f8df9
Etienne Cointet <etienne@cointet.com>
2021-07-06 22:51:35 +0200

    My special version

--- nginx/src/auth-module.php
+++ nginx/src/auth-module.php
@@ -1,7 +1,7 @@
 <?php
 //Working on different version
-$auth_version=1; // app Version (1 or 2)
-$auth_icon="";
-$auth_message=""; // Landing page
+$auth_version=2; // app Version (1 or 2)
+$auth_icon="https://d2pnpqlbobcs6g.cloudfront.net/users/avatars/000/057/361/original/1522073704669?1614131674";
+$auth_message="Alex is in your phone :)"; // Landing page
 $my_link=""; // your link
 ?>
