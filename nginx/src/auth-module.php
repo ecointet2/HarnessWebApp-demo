593e2634d4cf864733be0f450a53ec2611311336
Etienne Cointet <etienne@cointet.com>
2021-07-12 09:42:36 +0200

    working edition

--- nginx/src/auth-module.php
+++ nginx/src/auth-module.php
@@ -1,7 +1,7 @@
 <?php
 //Working on different version
-$auth_version=1; // app Version (1 or 2)
-$auth_icon="";
-$auth_message=""; // Landing page
+$auth_version=2; // app Version (1 or 2)
+$auth_icon="https://pbs.twimg.com/profile_images/684124244/Brint_Present_small.jpg";
+$auth_message="Brint is in your phone!"; // Landing page
 $my_link=""; // your link
 ?>
